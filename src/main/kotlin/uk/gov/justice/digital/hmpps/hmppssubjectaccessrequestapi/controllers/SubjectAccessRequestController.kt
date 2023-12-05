0686b58de58873b69c3f8d287136193a10c0cc36
CslBing <bingjie.liu@digital.justice.gov.uk>
2023-12-05 13:01:39 +0000

    Fix typo

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/controllers/SubjectAccessRequestController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/controllers/SubjectAccessRequestController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/")
 class SubjectAccessRequestController {
 
-  @PostMapping("creatSubjectAccessReport")
+  @PostMapping("createsSubjectAccessReport")
   fun createSubjectAccessReportPost(): String {
     return "MockId"
   }
