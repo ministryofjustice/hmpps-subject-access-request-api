6663a6ce6d83ccf30e9715c05656995abef5fda3
HorstmannA <anna.horstmann@madetech.com>
2024-01-02 15:53:08 +0000

    Update schema for local deployments to match that of the dev/prod/preprod deployments.

--- db/dev/schema.sql
+++ db/dev/schema.sql
@@ -1,19 +1,26 @@
-CREATE TABLE IF NOT EXISTS SubjectAccessRequest (
-            id VARCHAR(60) PRIMARY KEY,
-            status VARCHAR NOT NULL,
-            dateFrom DATE,
-            dateTo DATE NOT NULL,
-            sarCaseReferenceNumber VARCHAR NOT NULL,
-            services VARCHAR [] NOT NULL,
-            nomisId VARCHAR,
-            ndeliusCaseReferenceId VARCHAR,
-            hmppsId VARCHAR,
-            CHECK (nomisId IS NOT NULL OR ndeliusCaseReferenceId IS NOT NULL OR hmppsId IS NOT NULL),
-            subject VARCHAR NOT NULL,
-            requestedBy VARCHAR NOT NULL,
-            requestDateTime TIMESTAMP NOT NULL,
-            claimDateTime TIMESTAMP,
-            objectURL VARCHAR,
-            presignedURL VARCHAR,
-            claimAttempts SMALLINT
+CREATE TYPE status_enum AS ENUM('Pending', 'Completed');
+
+CREATE TABLE IF NOT EXISTS subject_access_request (
+      id SERIAL PRIMARY KEY,
+      status status_enum NOT NULL DEFAULT 'Pending',
+      date_from DATE,
+      date_to DATE NOT NULL DEFAULT CURRENT_DATE,
+      sar_case_reference_number TEXT NOT NULL,
+      services TEXT ARRAY NOT NULL,
+      nomis_id TEXT,
+      ndelius_case_reference_id TEXT,
+      hmpps_id TEXT,
+      subject TEXT NOT NULL,
+      requested_by TEXT NOT NULL,
+      request_date_time TIMESTAMP with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      claim_date_time TIMESTAMP with time zone,
+      object_url TEXT,
+      presigned_url TEXT,
+      claim_attempts SMALLINT DEFAULT 0
 );
+
+CREATE INDEX status_index ON subject_access_request (status);
+CREATE INDEX sar_case_reference_number_index ON subject_access_request (sar_case_reference_number);
+CREATE INDEX subject_index ON subject_access_request (subject);
+CREATE INDEX requested_by_index ON subject_access_request (requested_by);
+CREATE INDEX claim_attempts_index ON subject_access_request (claim_attempts);
