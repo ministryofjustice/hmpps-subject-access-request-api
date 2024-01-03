40362d49ba80c7de9cd9a423bfbb9ff3af5331c2
HorstmannA <anna.horstmann@madetech.com>
2024-01-03 12:57:20 +0000

    Lint.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/models/Report.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/models/Report.kt
@@ -1,32 +1,50 @@
 package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.models
 
-import jakarta.persistence.*
+import jakarta.persistence.ElementCollection
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import java.time.LocalDateTime
 
 @Entity
 data class Report(
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    val id: String,
-    val status: String,
-    val dateFrom: LocalDateTime? = null,
-    val dateTo: LocalDateTime? = null,
-    val sarCaseReferenceNumber: String,
-    @ElementCollection
-    val services: List<String>,
-    val nomisId: String?,
-    val ndeliusCaseReferenceId: String?,
-    val hmppsId: String?,
-    val subject: String,
-    val requestedBy: String,
-    val requestDateBySar: LocalDateTime? = null,
-    val claimDateTime: LocalDateTime? = null,
-    val objectURL: String?,
-    val presignedURL: String?,
-    val claimAttempts: Int,
-
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  val id: String,
+  val status: String,
+  val dateFrom: LocalDateTime? = null,
+  val dateTo: LocalDateTime? = null,
+  val sarCaseReferenceNumber: String,
+  @ElementCollection
+  val services: List<String>,
+  val nomisId: String?,
+  val ndeliusCaseReferenceId: String?,
+  val hmppsId: String?,
+  val subject: String,
+  val requestedBy: String,
+  val requestDateBySar: LocalDateTime? = null,
+  val claimDateTime: LocalDateTime? = null,
+  val objectURL: String?,
+  val presignedURL: String?,
+  val claimAttempts: Int,
 ) {
-  constructor() : this("", "", null, null, "", listOf("", ""),"", "", "", "",
-  "",
-    null, null, "", "", 0,)
+  constructor() : this(
+    "",
+    "",
+    null,
+    null,
+    "",
+    listOf("", ""),
+    "",
+    "",
+    "",
+    "",
+    "",
+    null,
+    null,
+    "",
+    "",
+    0,
+  )
 }
