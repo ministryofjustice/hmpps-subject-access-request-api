2e40b90f47ac41600ee5854593d134d8548f79a6
HorstmannA <anna.horstmann@madetech.com>
2024-01-03 12:49:57 +0000

    Revert Report model names and make it an Entity.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/models/Report.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/models/Report.kt
@@ -1,21 +1,32 @@
 package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.models
 
+import jakarta.persistence.*
 import java.time.LocalDateTime
+
+@Entity
 data class Report(
-  val id: String,
-  val status: String,
-  val date_from: LocalDateTime,
-  val date_to: LocalDateTime,
-  val sar_case_reference_number: String,
-  val services: List<String>,
-  val nomis_id: String?,
-  val ndelius_case_reference_id: String?,
-  val hmpps_id: String?,
-  val subject: String,
-  val requested_by: String,
-  val request_date_by_sar: LocalDateTime,
-  val claim_date_time: LocalDateTime,
-  val object_url: String?,
-  val presigned_url: String?,
-  val claim_attempts: Int,
-)
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: String,
+    val status: String,
+    val dateFrom: LocalDateTime? = null,
+    val dateTo: LocalDateTime? = null,
+    val sarCaseReferenceNumber: String,
+    @ElementCollection
+    val services: List<String>,
+    val nomisId: String?,
+    val ndeliusCaseReferenceId: String?,
+    val hmppsId: String?,
+    val subject: String,
+    val requestedBy: String,
+    val requestDateBySar: LocalDateTime? = null,
+    val claimDateTime: LocalDateTime? = null,
+    val objectURL: String?,
+    val presignedURL: String?,
+    val claimAttempts: Int,
+
+
+) {
+  constructor() : this("", "", null, null, "", listOf("", ""),"", "", "", "",
+  "",
+    null, null, "", "", 0,)
+}
