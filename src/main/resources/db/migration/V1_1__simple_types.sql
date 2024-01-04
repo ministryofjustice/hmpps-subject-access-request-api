cdb42d6aa061f2136b5fd7e7e1ff77faf8873223
HorstmannA <anna.horstmann@madetech.com>
2024-01-04 16:44:26 +0000

    Capitalise 'text' type in SQL.

--- src/main/resources/db/migration/V1_1__simple_types.sql
+++ src/main/resources/db/migration/V1_1__simple_types.sql
@@ -1,7 +1,7 @@
 ALTER TABLE subject_access_request
-    ALTER COLUMN status TYPE text,
+    ALTER COLUMN status TYPE TEXT,
     ALTER COLUMN status SET DEFAULT 'Pending',
-    ALTER COLUMN services TYPE text,
+    ALTER COLUMN services TYPE TEXT,
     DROP COLUMN hmpps_id,
     DROP COLUMN subject,
     DROP COLUMN presigned_url;
