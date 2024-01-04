3e048d10998f3a2e2caf3da53e72dd0217664046
HorstmannA <anna.horstmann@madetech.com>
2024-01-04 17:54:23 +0000

    Attempt fix SQL syntax error.

--- src/main/resources/db/migration/V1_1__simple_types.sql
+++ src/main/resources/db/migration/V1_1__simple_types.sql
@@ -1,7 +1,7 @@
-ALTER TABLE subject_access_request
-    ALTER COLUMN status SET DEFAULT 'Pending',
-    ALTER COLUMN services TYPE text,
-    DROP COLUMN hmpps_id,
-    DROP COLUMN subject,
-    DROP COLUMN presigned_url;
+ALTER TABLE subject_access_request ALTER COLUMN status TYPE text;
+ALTER TABLE subject_access_request ALTER COLUMN status SET DEFAULT 'Pending';
+ALTER TABLE subject_access_request ALTER COLUMN services TYPE text;
+ALTER TABLE subject_access_request DROP COLUMN hmpps_id;
+ALTER TABLE subject_access_request DROP COLUMN subject;
+ALTER TABLE subject_access_request DROP COLUMN presigned_url;
 DROP TYPE status_enum;
