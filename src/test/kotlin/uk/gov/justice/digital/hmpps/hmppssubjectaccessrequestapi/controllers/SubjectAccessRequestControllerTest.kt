f6514b4e4d58078de2d027515c2ce0e676ef6f0b
HorstmannA <anna.horstmann@madetech.com>
2024-01-05 13:32:33 +0000

    Almost fix test.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/controllers/SubjectAccessRequestControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/controllers/SubjectAccessRequestControllerTest.kt
@@ -2,15 +2,20 @@ package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.controllers
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.json.JSONObject
 import org.mockito.Mockito
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
-import org.mockito.Mockito.`when`
 import org.springframework.security.core.Authentication
 import org.springframework.security.test.context.support.WithUserDetails
+import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.models.Status
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.models.SubjectAccessRequest
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.repository.SubjectAccessRequestRepository
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.services.AuditService
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 class SubjectAccessRequestControllerTest {
   @Test
   @WithUserDetails("customUsername")
@@ -18,13 +23,35 @@ class SubjectAccessRequestControllerTest {
     val sarRepository = Mockito.mock(SubjectAccessRequestRepository::class.java)
     val auditService = Mockito.mock(AuditService::class.java)
     val authentication: Authentication = Mockito.mock(Authentication::class.java)
-    Mockito.`when`(authentication.getName()).thenReturn("aName")
+    Mockito.`when`(authentication.name).thenReturn("aName")
+    val request = "{ dateFrom: '01/12/2023', dateTo: '03/01/2024', sarCaseReferenceNumber: '1234abc', services: '{1,2,4}', nomisId: '1', ndeliusCaseReferenceId: '1' }"
 
     val expected: String = "MockId"
     val result: String = SubjectAccessRequestController(auditService, sarRepository)
-      .createSubjectAccessRequestPost("{ dateFrom: '01/12/2023', dateTo: '03/01/2024', sarCaseReferenceNumber: '1234abc', services: '{1,2,4}', nomisId: '1', ndeliusCaseReferenceId: '1' }", authentication)
+      .createSubjectAccessRequestPost(request, authentication)
 
-    verify(sarRepository, times(1)).save(SubjectAccessRequest())
+    var json = JSONObject(request)
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    val dateFrom = json.get("dateFrom").toString()
+    val dateFromFormatted = LocalDate.parse(dateFrom, formatter)
+
+    val dateTo = json.get("dateTo").toString()
+    val dateToFormatted = LocalDate.parse(dateTo, formatter)
+
+    verify(sarRepository, times(1)).save(
+      SubjectAccessRequest(
+        id = null,
+        status = Status.Pending,
+        dateFrom = dateFromFormatted,
+        dateTo = dateToFormatted,
+        sarCaseReferenceNumber = "1234abc",
+        services = "{1,2,4}",
+        nomisId = "1",
+        ndeliusCaseReferenceId = "1",
+        requestedBy = authentication.name,
+        requestDateTime = LocalDateTime.now(),
+      ),
+    )
     Assertions.assertThat(result).isEqualTo(expected)
   }
 }
