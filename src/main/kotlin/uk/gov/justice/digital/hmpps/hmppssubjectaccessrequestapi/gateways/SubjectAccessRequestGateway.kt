d6cb56336e0fe4e45520f3e00cddd4a664ce2fcd
AprilMD <95350189+AprilMD@users.noreply.github.com>
2024-02-01 17:23:21 +0000

    Add time logic to get subject access request gateway

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/gateways/SubjectAccessRequestGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/gateways/SubjectAccessRequestGateway.kt
@@ -2,21 +2,32 @@ package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.gateways
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.models.Status
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.models.SubjectAccessRequest
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.repository.SubjectAccessRequestRepository
+import java.time.LocalDateTime
+
 @Component
 class SubjectAccessRequestGateway(@Autowired val repo: SubjectAccessRequestRepository) {
-  fun getSubjectAccessRequests(unclaimedOnly: Boolean): List<SubjectAccessRequest?> {
+  fun getSubjectAccessRequests(unclaimedOnly: Boolean, currentTime: LocalDateTime = LocalDateTime.now()): List<SubjectAccessRequest?> {
     if (unclaimedOnly) {
+
+//      val subjectAccessRequests: List<SubjectAccessRequest?> = emptyList()
+
+      val sarsWithNoClaims = repo.findByClaimAttemptsIs(0)
+
+      val sarsWithExpiredClaims = repo.findByStatusIsAndClaimAttemptsGreaterThanAndClaimDateTimeBefore(Status.Pending, 0, currentTime.minusMinutes(5))
+
+
       // val expiredClaimDate = LocalDateTime.(Now.fiveMinutesAgo)
       // repo.findByStatusAndClaimAttemptsOrClaimDateTime(status: "pending", claimAttempts: "0")
-      val subjectAccessRequests: List<SubjectAccessRequest?> = repo.findByClaimAttemptsIs(0)
+
+//      val subjectAccessRequests: List<SubjectAccessRequest?> = repo.findByClaimAttemptsIs(0)
 //          status == pending
 //          AND
 //          claimAttempts == 0
 //          OR claimDateTime == before expiredClaimDate
 //
-      return subjectAccessRequests
     }
     val response = repo.findAll()
     return response
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/gateways/SubjectAccessRequestGatewayTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestapi/gateways/SubjectAccessRequestGatewayTest.kt
@@ -1,25 +1,53 @@
 package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.gateways
 
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
+import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.models.Status
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.models.SubjectAccessRequest
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestapi.repository.SubjectAccessRequestRepository
-class SubjectAccessRequestGatewayTest {
-  @Test
-  fun `gateway calls findAll if unclaimed is false`() {
-    val sarRepository = Mockito.mock(SubjectAccessRequestRepository::class.java)
-    val result: List<SubjectAccessRequest?> = SubjectAccessRequestGateway(sarRepository)
-      .getSubjectAccessRequests(unclaimedOnly = false)
-    verify(sarRepository, times(1)).findAll()
-  }
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-  @Test
-  fun `gateway calls findByClaimAttemptsIs if unclaimed is true`() {
-    val sarRepository = Mockito.mock(SubjectAccessRequestRepository::class.java)
-    val result: List<SubjectAccessRequest?> = SubjectAccessRequestGateway(sarRepository)
-      .getSubjectAccessRequests(unclaimedOnly = true)
-    verify(sarRepository, times(1)).findByClaimAttemptsIs(0)
+class SubjectAccessRequestGatewayTest {
+  @Nested
+  inner class getSubjectAccessRequests {
+    private val sarRepository = Mockito.mock(SubjectAccessRequestRepository::class.java)
+    private val dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")
+    @Test
+    fun `calls findAll if unclaimed is false`() {
+      val result = SubjectAccessRequestGateway(sarRepository)
+        .getSubjectAccessRequests(unclaimedOnly = false)
+      verify(sarRepository, times(1)).findAll()
+    }
+
+    @Test
+    fun `calls findByClaimAttemptsIs if unclaimed is true`() {
+      val result = SubjectAccessRequestGateway(sarRepository)
+        .getSubjectAccessRequests(unclaimedOnly = true)
+      verify(sarRepository, times(1)).findByClaimAttemptsIs(0)
+    }
+
+    @Test
+    fun `calls findByClaimAttemptsIs(0) if unclaimed is true`() {
+      val result = SubjectAccessRequestGateway(sarRepository)
+        .getSubjectAccessRequests(unclaimedOnly = true)
+      verify(sarRepository, times(1)).findByClaimAttemptsIs(0)
+    }
+
+    @Test
+    fun `calls findByStatusIsAndClaimAttemptsGreaterThanAndClaimDateTimeBefore if unclaimed is true`() {
+      val mockedCurrentTime = "02/01/2024 00:00"
+      val formattedMockedCurrentTime = LocalDateTime.parse(mockedCurrentTime, dateTimeFormatter)
+      val expiredClaimDateTime = "01/01/2024 23:55"
+      val expiredClaimDateTimeFormatted = LocalDateTime.parse(expiredClaimDateTime, dateTimeFormatter)
+
+      val result: List<SubjectAccessRequest?> = SubjectAccessRequestGateway(sarRepository)
+        .getSubjectAccessRequests(unclaimedOnly = true, formattedMockedCurrentTime)
+      verify(sarRepository, times(1)).findByStatusIsAndClaimAttemptsGreaterThanAndClaimDateTimeBefore(Status.Pending, 0, expiredClaimDateTimeFormatted)
+    }
+
   }
 }
