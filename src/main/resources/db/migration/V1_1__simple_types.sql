355767ccec4b3c59159b22db14b44be1980e04ba
Anna Horstmann <67278233+HorstmannA@users.noreply.github.com>
2024-02-20 17:37:48 +0000

    Revert schema - can't change migration files. (#34)

--- src/main/resources/db/migration/V1_1__simple_types.sql
+++ src/main/resources/db/migration/V1_1__simple_types.sql
@@ -1,7 +1,7 @@
 ALTER TABLE subject_access_request ALTER COLUMN status TYPE text;
 ALTER TABLE subject_access_request ALTER COLUMN status SET DEFAULT 'Pending';
 ALTER TABLE subject_access_request ALTER COLUMN services TYPE text;
-ALTER TABLE subject_access_request DROP COLUMN IF EXISTS hmpps_id;
-ALTER TABLE subject_access_request DROP COLUMN IF EXISTS subject;
-ALTER TABLE subject_access_request DROP COLUMN IF EXISTS presigned_url;
+ALTER TABLE subject_access_request DROP COLUMN hmpps_id;
+ALTER TABLE subject_access_request DROP COLUMN subject;
+ALTER TABLE subject_access_request DROP COLUMN presigned_url;
 DROP TYPE IF EXISTS status_enum;
