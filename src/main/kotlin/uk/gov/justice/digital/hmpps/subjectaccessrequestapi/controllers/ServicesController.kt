1c8a72307954a05ca9de8c74c11479f89cf941c7
Dave Llewellyn <david.llewellyn@digital.justice.gov.uk>
2025-01-29 09:55:52 +0000

    addressed review comments

--- src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/controllers/ServicesController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/controllers/ServicesController.kt
@@ -1,5 +1,10 @@
 package uk.gov.justice.digital.hmpps.subjectaccessrequestapi.controllers
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -17,6 +22,43 @@ class ServicesController(
 ) {
 
   @GetMapping
+  @Operation(
+    summary = "Get the Services list",
+    description = "Get the Services list",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Request successful",
+        content = [
+          Content(
+            mediaType = "application/json",
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden - user not authorised to get services list",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = String::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "500",
+        description = "Unable to serve request.",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = String::class),
+          ),
+        ],
+      ),
+    ],
+  )
   fun getServices(): ResponseEntity<List<ServiceInfo>> {
     val services: List<ServiceInfo>? = serviceConfigurationService.getServiceConfigurationSanitised()?.map {
       ServiceInfo(it.id, it.serviceName, it.label, it.url, it.order)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/services/ServiceConfigurationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/services/ServiceConfigurationService.kt
@@ -1,10 +1,10 @@
 package uk.gov.justice.digital.hmpps.subjectaccessrequestapi.services
 
-import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.models.ServiceConfiguration
 import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.repository.ServiceConfigurationRepository
 
-@Component
+@Service
 class ServiceConfigurationService(private val serviceConfigurationRepository: ServiceConfigurationRepository) {
   fun getServiceConfigurationSanitised(): List<ServiceConfiguration>? =
     serviceConfigurationRepository.findByOrderByOrderAsc()
