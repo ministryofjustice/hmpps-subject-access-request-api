f90eea5da348dd9c2b49d26261e16a6d576ff082
Inderjit Virdi <inderjit.virdi@digital.justice.gov.uk>
2025-03-13 14:31:11 +0000

    HAAR-3657: Add property for locations request page size

--- src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/client/LocationsClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/client/LocationsClient.kt
@@ -1,19 +1,19 @@
 package uk.gov.justice.digital.hmpps.subjectaccessrequestapi.client
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
-private const val SIZE = 200
-
 @Component
 class LocationsClient(
   private val locationsApiWebClient: WebClient,
+  @Value("\${application.locations-request.page-size}") private val pageSize: Int,
 ) {
 
   fun getLocationDetails(page: Int): LocationResults = locationsApiWebClient.get()
-    .uri("/locations", mapOf("page" to page, "size" to SIZE))
+    .uri("/locations", mapOf("page" to page, "size" to pageSize))
     .retrieve()
     .bodyToMono(object : ParameterizedTypeReference<LocationResults>() {})
     .block()!!
