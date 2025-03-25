6f6a0c5950d150b60287e93d06e59fc340d4f5d9
Inderjit Virdi <inderjit.virdi@digital.justice.gov.uk>
2025-03-25 10:46:50 +0000

    HAAR-3686: Update test for changes to service configuration data

--- src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/controllers/ServicesControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/controllers/ServicesControllerIntTest.kt
@@ -36,24 +36,24 @@ class ServicesControllerIntTest : IntegrationTestBase() {
       .jsonPath("$").isArray
       .jsonPath("$.length()").isEqualTo(26)
       .jsonPath("$[0].id").isNotEmpty
-      .jsonPath("$[0].name").isEqualTo("keyworker-api")
-      .jsonPath("$[0].label").isEqualTo("Keyworker")
-      .jsonPath("$[0].url").isEqualTo("https://keyworker-api-dev.prison.service.justice.gov.uk")
+      .jsonPath("$[0].name").isEqualTo("G1")
+      .jsonPath("$[0].label").isEqualTo("G1")
+      .jsonPath("$[0].url").isEqualTo("G1")
       .jsonPath("$[0].order").isEqualTo(1)
-      .jsonPath("$[1].id").isNotEmpty
-      .jsonPath("$[1].name").isEqualTo("offender-case-notes")
-      .jsonPath("$[1].label").isEqualTo("Sensitive Case Notes")
-      .jsonPath("$[1].url").isEqualTo("https://dev.offender-case-notes.service.justice.gov.uk")
-      .jsonPath("$[1].order").isEqualTo(2)
+      .jsonPath("$[3].id").isNotEmpty
+      .jsonPath("$[3].name").isEqualTo("hmpps-book-secure-move-api")
+      .jsonPath("$[3].label").isEqualTo("Book a Secure Move")
+      .jsonPath("$[3].url").isEqualTo("https://hmpps-book-secure-move-api-staging.apps.cloud-platform.service.justice.gov.uk")
+      .jsonPath("$[3].order").isEqualTo(4)
       .jsonPath("$[13].id").isNotEmpty
-      .jsonPath("$[13].name").isEqualTo("make-recall-decision-api")
-      .jsonPath("$[13].label").isEqualTo("Consider a Recall")
-      .jsonPath("$[13].url").isEqualTo("https://make-recall-decision-api-dev.hmpps.service.justice.gov.uk")
+      .jsonPath("$[13].name").isEqualTo("hmpps-non-associations-api")
+      .jsonPath("$[13].label").isEqualTo("Non-associations")
+      .jsonPath("$[13].url").isEqualTo("https://non-associations-api-dev.hmpps.service.justice.gov.uk")
       .jsonPath("$[13].order").isEqualTo(14)
       .jsonPath("$[25].id").isNotEmpty
-      .jsonPath("$[25].name").isEqualTo("G3")
-      .jsonPath("$[25].label").isEqualTo("G3")
-      .jsonPath("$[25].url").isEqualTo("G3")
+      .jsonPath("$[25].name").isEqualTo("make-recall-decision-api")
+      .jsonPath("$[25].label").isEqualTo("Consider a Recall")
+      .jsonPath("$[25].url").isEqualTo("https://make-recall-decision-api-dev.hmpps.service.justice.gov.uk")
       .jsonPath("$[25].order").isEqualTo(26)
   }
 
