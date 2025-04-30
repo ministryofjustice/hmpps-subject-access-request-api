8afaf38bc2eb5a647d6bb673d68aed983062642b
AlTurner-MOJ <101654848+AlTurner-MOJ@users.noreply.github.com>
2025-04-30 09:00:58 +0100

    HAAR-3898: ⬆️ update to spring-boot-plugin-8.1.0 (#147)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.3"
-  id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
-  kotlin("plugin.spring") version "2.1.10"
-  kotlin("plugin.jpa") version "2.1.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.1.0"
+  id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20"
+  kotlin("plugin.spring") version "2.1.20"
+  kotlin("plugin.jpa") version "2.1.20"
 }
 
 configurations {
@@ -16,25 +16,25 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.3.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.3")
   implementation("org.json:json:20250107")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
-  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.3.0")
-  implementation("io.sentry:sentry-logback:8.3.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.10.0")
+  implementation("io.sentry:sentry-logback:8.10.0")
 
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("com.h2database:h2:2.3.232")
   runtimeOnly("org.postgresql:postgresql:42.7.5")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.3.1")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.3")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation("org.wiremock:wiremock-standalone:3.12.1")
+  testImplementation("org.wiremock:wiremock-standalone:3.13.0")
 }
 
 kotlin {
