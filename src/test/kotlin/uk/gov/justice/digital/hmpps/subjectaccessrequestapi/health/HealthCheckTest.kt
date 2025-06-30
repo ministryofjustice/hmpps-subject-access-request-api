7ffcae03face22f0dfa3083244b5c7400128d434
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-06-30 16:29:05 +0100

    HAAR-3945: 👷try to stop Health page reports down failing due to timeout

--- src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/health/HealthCheckTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/health/HealthCheckTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.subjectaccessrequestapi.health
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.test.web.reactive.server.WebTestClient.BodyContentSpec
 import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.integration.IntegrationTestBase
 
@@ -30,6 +31,7 @@ class HealthCheckTest : IntegrationTestBase() {
       .componentHasStatusAndUrls("my-alt-dynamic-service", "UP", 8091, "sarServiceApis.details.")
   }
 
+  @AutoConfigureWebTestClient(timeout = "PT15S")
   @Test
   fun `Health page reports down`() {
     stubPingWithResponse(503)
