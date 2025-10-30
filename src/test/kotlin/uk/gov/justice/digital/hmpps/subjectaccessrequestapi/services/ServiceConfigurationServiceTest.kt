bbcd7fd5f52ea4e298b24d1e88e376e7709da7dc
Dave Llewellyn <david.llewellyn@digital.justice.gov.uk>
2025-10-30 08:59:22 +0000

    added flyway scripts to create new template_version table, added template_migrated field to service config

--- src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/services/ServiceConfigurationServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/services/ServiceConfigurationServiceTest.kt
@@ -11,12 +11,12 @@ import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.repository.ServiceCo
 
 class ServiceConfigurationServiceTest {
 
-  private val s1 = ServiceConfiguration(serviceName = "service1", label = "Service One", url = "s1.com", order = 1, enabled = true)
-  private val s2 = ServiceConfiguration(serviceName = "service2", label = "Service Two", url = "s2.com", order = 2, enabled = true)
-  private val s3 = ServiceConfiguration(serviceName = "service3", label = "Service Three", url = "s3.com", order = 3, enabled = true)
-  private val g1 = ServiceConfiguration(serviceName = "G1", label = "G1", url = "G1", order = 4, enabled = true)
-  private val g2 = ServiceConfiguration(serviceName = "G2", label = "G2", url = "G2", order = 5, enabled = true)
-  private val g3 = ServiceConfiguration(serviceName = "G3", label = "G3", url = "G3", order = 5, enabled = true)
+  private val s1 = ServiceConfiguration(serviceName = "service1", label = "Service One", url = "s1.com", order = 1, enabled = true, templateMigrated = false)
+  private val s2 = ServiceConfiguration(serviceName = "service2", label = "Service Two", url = "s2.com", order = 2, enabled = true, templateMigrated = false)
+  private val s3 = ServiceConfiguration(serviceName = "service3", label = "Service Three", url = "s3.com", order = 3, enabled = true, templateMigrated = false)
+  private val g1 = ServiceConfiguration(serviceName = "G1", label = "G1", url = "G1", order = 4, enabled = true, templateMigrated = false)
+  private val g2 = ServiceConfiguration(serviceName = "G2", label = "G2", url = "G2", order = 5, enabled = true, templateMigrated = false)
+  private val g3 = ServiceConfiguration(serviceName = "G3", label = "G3", url = "G3", order = 5, enabled = true, templateMigrated = false)
 
   private val serviceConfigurationRepository: ServiceConfigurationRepository = mock()
   private val service = ServiceConfigurationService(serviceConfigurationRepository)
