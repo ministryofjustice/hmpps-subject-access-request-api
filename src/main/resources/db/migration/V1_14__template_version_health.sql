55bd5cf443c032ada1763b31b8b4f2911d019e26
Dave Llewellyn <david.llewellyn@digital.justice.gov.uk>
2026-01-13 14:22:17 +0000

    added flyway script for template version health and shedlock tables

---
+++ src/main/resources/db/migration/V1_14__template_version_health.sql
@@ -0,0 +1,19 @@
+-- Table to persist the template version health check results.
+CREATE TABLE IF NOT EXISTS template_version_health_status
+(
+    id                       UUID PRIMARY KEY,
+    service_configuration_id UUID                     NOT NULL,
+    status                   TEXT                     NOT NULL,
+    last_modified            TIMESTAMP with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    constraint fk_service_configuration foreign key (service_configuration_id) REFERENCES service_configuration (id)
+);
+
+-- Use shedlock to ensure scheduled tasks only runs in single instance. When the service is deployed in an environment
+-- there are will be multiple pods - we only want tasks to run once per interval not once per pod per interval.
+CREATE TABLE shedlock
+(
+    name       VARCHAR(64)  NOT NULL PRIMARY KEY,
+    lock_until TIMESTAMP(3) NULL,
+    locked_at  TIMESTAMP(3) NULL,
+    locked_by  VARCHAR(255) NULL
+);
