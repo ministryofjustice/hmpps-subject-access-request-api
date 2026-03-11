4b4846bca4bb1c4a8337bbc1d7867016394dcc78
indyv-moj <inderjit.virdi@digital.justice.gov.uk>
2026-03-11 17:00:03 +0000

    HAAR-5040: Add service level granularity for sar records (#229)
    * HAAR-5040: Add service level granularity for sar records
    
    * HAAR-5040: Link sar service level detail to template version entities and fix tests
    
    * HAAR-5040: Update test data setup to use list instead of mutablelist
    
    * HAAR-5040: Remove unused service config repository methods
    
    * HAAR-5040: Address review comments
    
    * HAAR-5040: Allow for suspended render status

--- src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/controllers/SubjectAccessRequestControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestapi/controllers/SubjectAccessRequestControllerIntTest.kt
@@ -1,20 +1,60 @@
 package uk.gov.justice.digital.hmpps.subjectaccessrequestapi.controllers
 
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.controllers.entity.CreateSubjectAccessRequestEntity
 import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.models.ServiceCategory
+import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.models.ServiceConfiguration
+import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.repository.ServiceConfigurationRepository
+import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.repository.SubjectAccessRequestRepository
+import uk.gov.justice.digital.hmpps.subjectaccessrequestapi.repository.TemplateVersionRepository
 import java.time.LocalDate
 
 class SubjectAccessRequestControllerIntTest : IntegrationTestBase() {
 
+  @Autowired
+  private lateinit var subjectAccessRequestRepository: SubjectAccessRequestRepository
+
+  @Autowired
+  private lateinit var serviceConfigurationRepository: ServiceConfigurationRepository
+
+  @Autowired
+  private lateinit var templateVersionRepository: TemplateVersionRepository
+
   private val createSubjectAccessRequest = CreateSubjectAccessRequestEntity(
     nomisId = "A1111AA",
     ndeliusId = null,
-    services = "service1, .com",
+    services = listOf("service1"),
     sarCaseReferenceNumber = "mockedCaseReference",
     dateTo = LocalDate.of(2022, 12, 25),
     dateFrom = LocalDate.of(2001, 1, 1),
   )
+
+  private val serviceConfigOne = ServiceConfiguration(
+    serviceName = "service1",
+    label = "Service One",
+    url = "http://service-one",
+    enabled = true,
+    templateMigrated = true,
+    category = ServiceCategory.PRISON,
+  )
+
+  @BeforeEach
+  fun setup() {
+    subjectAccessRequestRepository.deleteAll()
+    templateVersionRepository.deleteAll()
+    serviceConfigurationRepository.saveAll(listOf(serviceConfigOne))
+  }
+
+  @AfterEach
+  fun tearDown() {
+    subjectAccessRequestRepository.deleteAll()
+    templateVersionRepository.deleteAll()
+    serviceConfigurationRepository.deleteAllById(listOf(serviceConfigOne.id))
+  }
 
   @Test
   fun `should return unauthorized if no token`() {
